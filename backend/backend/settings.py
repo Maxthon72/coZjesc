ce8aad5b6ea074b0d1d8a079f14c685123c74e23
Maxthon72 <maciekageof@gmail.com>
2024-11-16 19:00:54 +0100

    nieudana proba azure, polaczenie z postgresql lokalnie

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import Config, RepositoryEnv
+import dj_database_url
+
+config = Config(RepositoryEnv('backend.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-35yo1d)4f$x-*uzjqx-l-ckrq#!zbq91(ew(gbq6irqzy^7_$_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -76,16 +75,27 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.parse(config('DATABASE_URL'))
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'co_zjesc',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
+# Spoonacular API Key
+SPOONACULAR_API_KEY = config('SPOONACULAR_API_KEY')
+
+# Secret Key
+SECRET_KEY = config('SECRET_KEY')
+
+# Debug Mode
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 CORS_ALLOW_ALL_ORIGINS = True
 
