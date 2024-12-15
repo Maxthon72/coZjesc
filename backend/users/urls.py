dab21a9cf44979017981e78ab0dc7a00dc22cee4
Maxthon72 <maciekageof@gmail.com>
2024-12-15 15:30:52 +0100

    Refresh token

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import RegisterView, LoginView, CreateStaffUserView,SuperuserLoginView, CheckIfLogedIn,UserInfoView
-
+from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('superuser-login/', SuperuserLoginView.as_view(), name='superuser-login'),
     path('check-if-loggedin/', CheckIfLogedIn.as_view(), name='check-if-loggedin'),
     path('user-info/', UserInfoView.as_view(), name='user-info'),
+    path('refresh-token/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
