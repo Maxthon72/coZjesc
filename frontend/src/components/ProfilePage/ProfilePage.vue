4be58ae513cd15d35700bcf35c50f362e3b3b4c0
Maxthon72 <maciekageof@gmail.com>
2024-12-15 13:22:02 +0100

    TempPage i zakłądki na stronie urzytkownika

--- frontend/src/components/ProfilePage/ProfilePage.vue
+++ frontend/src/components/ProfilePage/ProfilePage.vue
@@ -11,19 +11,35 @@
 
       <div v-else>
         <h1 class="title has-text-centered">User Profile</h1>
-        <div class="box">
-          <p><strong>Username:</strong> {{ user.username }}</p>
-          <p><strong>Email:</strong> {{ user.email }}</p>
-          <p><strong>First Name:</strong> {{ user.first_name }}</p>
-          <p><strong>Last Name:</strong> {{ user.last_name }}</p>
-          <p><strong>Staff:</strong> {{ user.is_staff ? "Yes" : "No" }}</p>
+        <!-- Tabs -->
+        <div class="tabs is-centered is-boxed">
+          <ul>
+            <li :class="{ 'is-active': activeTab === 'info' }" @click="activeTab = 'info'">
+              <a>User Info</a>
+            </li>
+            <li :class="{ 'is-active': activeTab === 'temp' }" @click="activeTab = 'temp'">
+              <a>Placeholder Page</a>
+            </li>
+          </ul>
+        </div>
+
+        <!-- Tab Content -->
+        <div>
+          <div v-if="activeTab === 'info'" class="box">
+            <p><strong>Username:</strong> {{ user.username }}</p>
+            <p><strong>Email:</strong> {{ user.email }}</p>
+            <p><strong>First Name:</strong> {{ user.first_name }}</p>
+            <p><strong>Last Name:</strong> {{ user.last_name }}</p>
+            <p><strong>Staff:</strong> {{ user.is_staff ? "Yes" : "No" }}</p>
+          </div>
+          <div v-else-if="activeTab === 'temp'">
+            <TempPage />
+          </div>
         </div>
       </div>
     </section>
   </div>
 </template>
-  
-  <script src="./ProfilePage.js"></script>
+<script src="./ProfilePage.js"></script>
 
-  <style src="./ProfilePage.css"></style>
-  
+<style src="./ProfilePage.css"></style>
---
+++ frontend/src/components/TempPage/TempPage.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container is-max-desktop temp-page">
+      <section class="section">
+        <div class="notification is-warning has-text-centered">
+          <h1 class="title is-3">Under Construction</h1>
+          <p class="subtitle is-5">
+            This page is not yet implemented. Check back later!
+          </p>
+          <router-link class="button is-primary is-rounded mt-4" to="/">
+            Return to Home
+          </router-link>
+        </div>
+      </section>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "TempPage",
+  };
+  </script>
+  
+  <style scoped>
+  .temp-page {
+    margin-top: 4rem;
+    text-align: center;
+  }
+  
+  .notification {
+    background-color: #ffdd57; /* Warning yellow */
+    color: #363636; /* Dark text */
+  }
+  
+  .button {
+    margin-top: 1rem;
+  }
+  </style>
+  
